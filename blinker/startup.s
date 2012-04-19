0a1c900b9144d53a83c6e32858eb81c11d22b475
David Welch <dwelch@dwelch.com>
2012-04-19 17:53:22 -0400

    building with llvm as well as gcc

--- blinker/startup.s
+++ blinker/startup.s
@@ -1,3 +1,4 @@
+.set    noreorder
 
 
 .globl _start
@@ -7,29 +8,25 @@ _start:
     li $17,2
 
     li $sp,0x1000
-    .set    noreorder
     jal notmain
     li $18,3
-    .set    reorder
 hang:
     j hang
     nop
 
 .globl PUT32
 PUT32:
-    .set    noreorder
     jr $ra
     sw $a1,0($a0)
-    .set    reorder
 
 .globl GET32
 GET32:
-    .set    noreorder
     jr $ra
     lw $v0,0($a0)
-    .set    reorder
 
 .globl dummy
 dummy:
     jr $ra
     nop
+
+.set    reorder
